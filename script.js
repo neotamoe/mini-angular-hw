52e112cea06b0f3b0c397fb871d02554b0626425
Neota Moe <neota.moe@gmail.com>
2017-05-08 19:33:13 -0500

    angular mini homework practice

---
+++ script.js
@@ -0,0 +1,53 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('fourIsMore', function(){
+  // view model
+  var vm = this;
+
+  this.uno = 'four legs on a dog';
+  this.dos = 'four musicians in a string quartet';
+
+  this.tres = '';
+
+});
+
+// practice repetition to try to commit initial syntax to memory
+// practice #3
+// var myApp = angular.module('myApp', []);
+//
+// myApp.controller('thirdTimeIsTheCharm', function(){
+//   //view model
+//   var vm = this;
+//
+//   this.one = 'Finding Dory';
+//   this.two = 'Finding Nemo';
+//
+//   this.favorite = 'type your favorite movie here';
+// });
+
+
+// practice 2
+// var myApp = angular.module('myApp', []);
+//
+// myApp.controller('secondRunController', function(){
+//
+//   //view model
+//   var vm = this;
+//
+//   this.thingOne = 'Try try again.';
+//   this.thingTwo = 'I think it is okay to repeat myself in practice.';
+//
+//   this.name = '';
+// });
+
+// practice 1
+// var myApp = angular.module('myApp', []);
+//
+// myApp.controller ('myFirstOwnController', function(){
+//   // view model
+//   var vm = this;
+//
+//   vm.greeting = 'Hello Angular World!';
+//   vm.thingOne = 'I really want pretzel chips, but am eating peeps instead.';
+//   vm.name = 'Violet';
+// });
